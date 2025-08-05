a240d6f766edc1166b5a4dea18aebd10161a8ca4
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-05 13:11:58 +0100

    made header and navbutton components and updated layout accordingly

--- src/app/(rs)/layout.tsx
+++ src/app/(rs)/layout.tsx
@@ -1,3 +1,5 @@
+import { Header } from "@/components/Header";
+
 interface RSLayoutProps {
   children: React.ReactNode;
 }
@@ -5,7 +7,7 @@ interface RSLayoutProps {
 export default async function RSLayout({ children }: RSLayoutProps) {
   return (
     <div className="mx-auto w-full max-w-7xl">
-      {/* Header */}
+      <Header />
       <div className="px-4 py-2">{children}</div>
     </div>
   );
