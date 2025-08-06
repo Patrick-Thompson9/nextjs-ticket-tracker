9f50deccc4c52f9f8bba17dbc425edd0a823e2c0
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-06 13:06:03 +0100

    added schema and index files

---
+++ src/db/schema.ts
@@ -0,0 +1,59 @@
+import {
+  pgTable,
+  serial,
+  varchar,
+  boolean,
+  timestamp,
+  integer,
+  text,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { title } from "process";
+
+export const customers = pgTable("customers", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("first_name", { length: 50 }).notNull(),
+  lastName: varchar("last_name", { length: 50 }).notNull(),
+  email: varchar("email", { length: 100 }).notNull().unique(),
+  phone: varchar("phone", { length: 15 }).notNull().unique(),
+  address1: varchar("address1", { length: 100 }).notNull(),
+  address2: varchar("address2", { length: 100 }),
+  city: varchar("city", { length: 50 }).notNull(),
+  state: varchar("state", { length: 2 }).notNull(),
+  postcode: varchar("Postcode", { length: 10 }).notNull(),
+  notes: text("notes"),
+  active: boolean("active").default(true).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const tickets = pgTable("tickets", {
+  id: serial("id").primaryKey(),
+  customerId: integer("customer_id")
+    .notNull()
+    .references(() => customers.id, { onDelete: "cascade" }),
+  title: varchar("title", { length: 500 }).notNull(),
+  description: text("description"),
+  completed: boolean("completed").default(false).notNull(),
+  tech: varchar("tech", { length: 50 }).notNull().default("Unassigned"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+// Relations
+export const customersRelations = relations(customers, ({ many }) => ({
+  tickets: many(tickets),
+}));
+
+export const ticketsRelations = relations(tickets, ({ one }) => ({
+  customer: one(customers, {
+    fields: [tickets.customerId],
+    references: [customers.id],
+  }),
+}));
