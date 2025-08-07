7745a9185f1f22ffc0e31133548f4a59809f0ae2
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-07 12:35:10 +0100

    imported db schema and passed it to drizzle db so query API works if needed.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,14 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
+import * as schema from "./schema";
 
 config({ path: ".env.local" });
 
 const sql = neon(process.env.DATABASE_URL!);
 
 // Logger
-// const db = drizzle(sql, { logger:true });
-const db = drizzle(sql);
+// const db = drizzle(sql, { logger:true, schema });
+const db = drizzle(sql, { schema });
 
 export { db };
