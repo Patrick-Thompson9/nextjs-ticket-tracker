4bb48b0f23235c7803994731af86d7074cc21abc
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-07 13:40:41 +0100

    made back button component

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { ButtonHTMLAttributes } from "react";
+
+type Props = {
+  title: string;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function BackButton({
+  title,
+  className,
+  variant,
+  ...props
+}: Props) {
+  const router = useRouter();
+
+  return (
+    <Button
+      variant={variant}
+      className={className}
+      onClick={() => router.back()}
+      title={title}
+      aria-label={props["aria-label"] || "Go back"}
+      {...props}
+    >
+      {title}
+    </Button>
+  );
+}
