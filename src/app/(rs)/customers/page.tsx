eb9ccecfd2b6c1401479123cb3331cec2838ef46
PatrickThompson9 <patrick@aoms-tech.com>
2025-09-04 10:55:51 +0100

    Added search functionality to the customers page

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,26 @@
+import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+
 export const metadata = {
-  title: "Customers",
+  title: "Customer Search",
 };
 
-export default function Customers() {
-  return <h2>Customers page</h2>;
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) return <CustomerSearch />;
+
+  const results = await getCustomerSearchResults(searchText);
+  console.log(results);
+
+  return (
+    <>
+      <CustomerSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 }
