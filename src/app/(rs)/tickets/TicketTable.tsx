686c1c2f5884a8d1300e5497b5e68edbba5de23a
PatrickThompson9 <patrick@aoms-tech.com>
2025-09-10 15:36:29 +0100

    added useEffect to refresh page number when the filters change and added page 1 minimum

--- src/app/(rs)/tickets/TicketTable.tsx
+++ src/app/(rs)/tickets/TicketTable.tsx
@@ -33,7 +33,7 @@ import {
 } from "lucide-react";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMemo } from "react";
 import { usePolling } from "@/hooks/usePolling";
 import { Button } from "@/components/ui/button";
@@ -171,6 +171,17 @@ export default function TicketTable({ data }: Props) {
     getFacetedUniqueValues: getFacetedUniqueValues(),
     getSortedRowModel: getSortedRowModel(),
   });
+
+  useEffect(() => {
+    const currentPageIndex = table.getState().pagination.pageIndex;
+    const pageCount = table.getPageCount();
+
+    if (pageCount <= currentPageIndex && currentPageIndex > 0) {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set("page", "1");
+      router.replace(`?${params.toString()}`, { scroll: false });
+    }
+  }, [table.getState().columnFilters]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className="mt-6 flex flex-col gap-4">
