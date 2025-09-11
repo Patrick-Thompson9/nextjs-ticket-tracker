47c21921d3ec1a086035191eccb777d4031c9d2e
PatrickThompson9 <patrick@aoms-tech.com>
2025-09-11 10:37:42 +0100

    added classes to the login page to make it more user friendly

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,11 +3,14 @@ import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
-    <main className="h-dvh flex flex-col items-center gap-6 text-4xl p-4">
-      <h1>ticket tracker</h1>
-      <Button asChild>
-        <LoginLink>Sign In</LoginLink>
-      </Button>
+    <main className="h-dvh flex flex-col items-center mt-10 p-10">
+      <div className="flex flex-col w-2/3 text-center items-center justify-between gap-10 p-5 bg-gray-500/30 rounded-sm">
+        <h1 className="text-6xl">Welcome to the Ticket Tracker</h1>
+        <hr className="border border-black/30 w-full" />
+        <Button className="w-full" asChild>
+          <LoginLink>Sign In</LoginLink>
+        </Button>
+      </div>
     </main>
   );
 }
