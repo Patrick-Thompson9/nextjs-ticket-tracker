de9e64f765bcc3a6859bdce21510bc251fae5ccf
PatrickThompson9 <patrick@aoms-tech.com>
2025-09-11 10:47:21 +0100

    fixed the login window pane sizing to properly account for screen size

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function LoginPage() {
   return (
     <main className="h-dvh flex flex-col items-center mt-10 p-10">
-      <div className="flex flex-col w-1/3 md:w-2/3 text-center items-center justify-between gap-10 p-5 bg-gray-500/30 rounded-sm">
+      <div className="flex flex-col lg:w-1/3 w-2/3 text-center items-center justify-between gap-10 p-5 bg-gray-500/30 rounded-sm">
         <h1 className="text-6xl">Welcome to the Ticket Tracker</h1>
         <hr className="border border-black/30 w-full" />
         <Button className="w-full" asChild>
